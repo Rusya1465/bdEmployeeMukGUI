f9cc69fc20f4cbe7536af5b456dd7bab9f027e9f
Rusya <85542333+Rusya1465@users.noreply.github.com>
2024-05-24 21:51:58 +0600

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿using System;
+
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvEmployees = new System.Windows.Forms.DataGridView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnAddEmployee = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.Label();
+            this.txtMiddleName = new System.Windows.Forms.Label();
+            this.txtPosition = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEmployees
+            // 
+            this.dgvEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployees.Location = new System.Drawing.Point(190, 42);
+            this.dgvEmployees.Name = "dgvEmployees";
+            this.dgvEmployees.RowHeadersWidth = 51;
+            this.dgvEmployees.RowTemplate.Height = 24;
+            this.dgvEmployees.Size = new System.Drawing.Size(934, 491);
+            this.dgvEmployees.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(15, 129);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(118, 57);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Найти Сотрудника";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click_1);
+            // 
+            // btnAddEmployee
+            // 
+            this.btnAddEmployee.Location = new System.Drawing.Point(1006, 619);
+            this.btnAddEmployee.Name = "btnAddEmployee";
+            this.btnAddEmployee.Size = new System.Drawing.Size(118, 57);
+            this.btnAddEmployee.TabIndex = 2;
+            this.btnAddEmployee.Text = "Добавить Сотрудника";
+            this.btnAddEmployee.UseVisualStyleBackColor = true;
+            this.btnAddEmployee.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 564);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Имя";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.AutoSize = true;
+            this.txtPhone.Location = new System.Drawing.Point(187, 635);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(121, 16);
+            this.txtPhone.TabIndex = 4;
+            this.txtPhone.Text = "Номер Телефона";
+            // 
+            // txtMiddleName
+            // 
+            this.txtMiddleName.AutoSize = true;
+            this.txtMiddleName.Location = new System.Drawing.Point(417, 564);
+            this.txtMiddleName.Name = "txtMiddleName";
+            this.txtMiddleName.Size = new System.Drawing.Size(66, 16);
+            this.txtMiddleName.TabIndex = 5;
+            this.txtMiddleName.Text = "Фамилия";
+            // 
+            // txtPosition
+            // 
+            this.txtPosition.AutoSize = true;
+            this.txtPosition.Location = new System.Drawing.Point(417, 635);
+            this.txtPosition.Name = "txtPosition";
+            this.txtPosition.Size = new System.Drawing.Size(78, 16);
+            this.txtPosition.TabIndex = 6;
+            this.txtPosition.Text = "Должность";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.AutoSize = true;
+            this.txtLastName.Location = new System.Drawing.Point(651, 564);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(70, 16);
+            this.txtLastName.TabIndex = 7;
+            this.txtLastName.Text = "Отчество";
+            this.txtLastName.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.AutoSize = true;
+            this.txtEmail.Location = new System.Drawing.Point(651, 635);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(41, 16);
+            this.txtEmail.TabIndex = 8;
+            this.txtEmail.Text = "Email";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(190, 583);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(172, 22);
+            this.txtFirstName.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(190, 654);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(172, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(420, 583);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(172, 22);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(420, 654);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(172, 22);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(654, 583);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(172, 22);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(654, 654);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(172, 22);
+            this.textBox6.TabIndex = 14;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(33, 219);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 22);
+            this.txtSearch.TabIndex = 15;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1163, 740);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtPosition);
+            this.Controls.Add(this.txtMiddleName);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAddEmployee);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.dgvEmployees);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvEmployees;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnAddEmployee;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtPhone;
+        private System.Windows.Forms.Label txtMiddleName;
+        private System.Windows.Forms.Label txtPosition;
+        private System.Windows.Forms.Label txtLastName;
+        private System.Windows.Forms.Label txtEmail;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox txtSearch;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dgvEmployees.DataSource = database.employees;
+        }
+
+        EmployeeDatabase database = new EmployeeDatabase();
+
+        private void btnRemoveEmployee_Click(object sender, EventArgs e)
+        {
+            if (dgvEmployees.SelectedRows.Count > 0)
+            {
+                Employee selectedEmployee = (Employee)dgvEmployees.SelectedRows[0].DataBoundItem;
+                database.RemoveEmployee(selectedEmployee.LastName, selectedEmployee.FirstName, selectedEmployee.MiddleName);
+                RefreshDataGridView();
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите сотрудника для удаления.", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            database.LoadData();
+            RefreshDataGridView();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            database.SaveData();
+        }
+
+        private void RefreshDataGridView()
+        {
+            dgvEmployees.DataSource = null;
+            dgvEmployees.DataSource = database.employees;
+        }
+
+        private void ClearInputFields()
+        {
+            txtLastName.Text = "";
+            txtFirstName.Text = "";
+            txtMiddleName.Text = "";
+            txtPosition.Text = "";
+            txtPhone.Text = "";
+            txtEmail.Text = "";
+        }
+
+       
+        [Serializable]
+        public class Employee
+        {
+            public string LastName { get; set; }
+            public string FirstName { get; set; }
+            public string MiddleName { get; set; }
+            public string Position { get; set; }
+            public string Phone { get; set; }
+            public string Email { get; set; }
+
+            public Employee(string lastName, string firstName, string middleName, string position, string phone, string email)
+            {
+                LastName = lastName;
+                FirstName = firstName;
+                MiddleName = middleName;
+                Position = position;
+                Phone = phone;
+                Email = email;
+            }
+
+            public Employee() { }
+
+            public override string ToString()
+            {
+                return $"{LastName} {FirstName} {MiddleName}, {Position}, tel.: {Phone}, e-mail: {Email}";
+            }
+        }
+
+        
+        class EmployeeDatabase
+        {
+            public List<Employee> employees = new List<Employee>();
+            private string fileName = "employees.xml";
+
+            public void AddEmployee(Employee employee)
+            {
+                employees.Add(employee);
+                SaveData();
+            }
+
+            public void RemoveEmployee(string lastName, string firstName, string middleName)
+            {
+                employees.RemoveAll(e => e.LastName == lastName && e.FirstName == firstName && e.MiddleName == middleName);
+                SaveData();
+            }
+
+            public List<Employee> FindEmployee(string query)
+            {
+                List<Employee> results = new List<Employee>();
+                foreach (var employee in employees)
+                {
+                    if (employee.LastName.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                        employee.FirstName.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                        employee.MiddleName.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                        employee.Position.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                        employee.Phone.IndexOf(query) >= 0 ||
+                        employee.Email.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        results.Add(employee);
+                    }
+                }
+                return results;
+            }
+
+            public void LoadData()
+            {
+                if (File.Exists(fileName))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(List<Employee>));
+                    using (StreamReader reader = new StreamReader(fileName))
+                    {
+                        employees = (List<Employee>)serializer.Deserialize(reader);
+                    }
+                }
+            }
+
+            public void SaveData()
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Employee>));
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    serializer.Serialize(writer, employees);
+                }
+            }
+        }
+
+        private void btnSearch_Click_1(object sender, EventArgs e)
+        {
+            string query = txtSearch.Text;
+            List<Employee> searchResults = database.FindEmployee(query);
+
+            dgvEmployees.DataSource = null;
+            dgvEmployees.DataSource = searchResults;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string lastName = txtLastName.Text;
+            string firstName = txtFirstName.Text;
+            string middleName = txtMiddleName.Text;
+            string position = txtPosition.Text;
+            string phone = txtPhone.Text;
+            string email = txtEmail.Text;
+
+            if (string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(firstName))
+            {
+                MessageBox.Show("Пожалуйста, заполните поля 'Фамилия' и 'Имя'", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Employee employee = new Employee(lastName, firstName, middleName, position, phone, email);
+            database.AddEmployee(employee);
+
+            RefreshDataGridView();
+            ClearInputFields();
+        }
+    }
+}
